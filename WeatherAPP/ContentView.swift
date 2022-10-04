d050de070385ce0c0c0fe8af73562cf4690b9b11
youngslol <hy.l@kakao.com>
2022-10-05 01:39:57 +0900

    Initial Commit

---
+++ WeatherAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherAPP
+//
+//  Created by 이호영 on 2022/10/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherAPP/WeatherAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPPApp.swift
+//  WeatherAPP
+//
+//  Created by 이호영 on 2022/10/05.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
